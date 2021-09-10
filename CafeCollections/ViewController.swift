49f18c447f274507ecee7a19372be896c25c2d65
Ella Madalinski <ella.madalinski24@d155.org>
2021-09-10 08:11:11 -0500

    Initial Commit

---
+++ CafeCollections/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  CafeCollections
+//
+//  Created by ELLA MADALINSKI on 9/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+//    var storeItems : [String:Int] = ["Strawberries":5, "Bananas":4, "Blueberries":3, "Mango":2, "Kiwi":1]
+//    var cart : [String:Int]
+    
+    
+    @IBOutlet weak var addItemTextFieldOutlet: UITextField!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+    }
+    
+    
+    @IBAction func addItemButton(_ sender: UIButton) {
+        
+//        let item = addItemTextFieldOutlet.text!
+//
+//        if item = "Strawberries"{
+//            cart[item] = 5
+//        }
+//        else if let item = storeItems["Bananas"] {
+//
+//        }
+//        else if let item = storeItems["Blueberries"]{
+//
+//        }
+//        else if let item = storeItems["Mango"] {
+//
+//        }
+//        else if let item = storeItems["Kiwi"] {
+//
+//        }
+//        else{
+//
+//        }
+        
+    }
+    
+    
+}
+
